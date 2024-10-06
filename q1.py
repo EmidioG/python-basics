4bfd62c34f60dd7aa000dd91fbb17d388b48ea3c
EmidioG <121321941+EmidioG@users.noreply.github.com>
2024-10-06 14:30:52 -0300

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+# Q1. Faça um algoritmo que lê dois números,
+#  e verifica se os dois números são iguais.
+#   Se forem iguais, escrever "São iguais", 
+#   se não, escrever "Não são iguais".
+n1 = int(input('digite o primeiro número\n'))
+n2 = int(input('digite o segundo número\n'))
+if(n1==n2):
+    print('os números são iguais')
+else:
+    print('os números são diferentes')
